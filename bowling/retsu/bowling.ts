bab26cdd493532e36f6164b0182df97ad82d3232
cl-r-yamamoto <r-yamamoto@creationline.com>
2026-08-30 18:23:19 +0900

    テストを軸に実装コードのリファクタリング。テストは変えない

--- bowling/retsu/bowling.ts
+++ bowling/retsu/bowling.ts
@@ -1,42 +1,73 @@
-type Point = number | null;
-type Frame = [Point, Point];
-type FinishedFrame = [number, number] | [10, null];
+type Frame = [number, number | null];
 
 type CalcResult = number | "pending";
 export function frameCalc(frames: Frame[]): CalcResult {
-  const firstFrame = frames[0];
+  if (frames[0] === undefined) return "pending"
+  const firstFrame = determineFrameState(frames[0]);
+
+  if (firstFrame.type === 'notRolled') {
+    return 'pending'
+  }
 
   // ストライクの時
-  if (firstFrame !== undefined && firstFrame[0] === 10) {
-    const secondFrame = frames[1];
-    if (isFrameFinished(secondFrame) === false) {
-      return "pending";
+  if (firstFrame.type === 'strike') {
+    if (frames[1] === undefined) return "pending"
+    const secondFrame = determineFrameState(frames[1]);
+    if (secondFrame.type === 'open' || secondFrame.type === 'spare') {
+      return 10 + secondFrame.first + secondFrame.second
     }
-    return 10 + secondFrame[0] + secondFrame[1];
-  }
-  // まだ未投入のとき
-  if (isFrameFinished(firstFrame) === false) {
     return "pending";
   }
-  const result = firstFrame[0] + firstFrame[1];
-
-  // スペアの時
-  if (result === 10) {
-    const secondFrame = frames[1];
-    // 次のフレームが確定していないとき
-    if (isFrameFinished(secondFrame) === false) {
-      return "pending";
-    }
-    return result + secondFrame[0];
+  // まだ未投入のとき
+  if (firstFrame.type === 'inProgress') {
+    return "pending";
+  }
+  if (firstFrame.type === 'open') {
+    return firstFrame.first + firstFrame.second
   }
 
-  return result;
+  // スペアの時
+  if (firstFrame.type === 'spare') {
+    if (frames[1] === undefined) return "pending"
+    const secondFrame = determineFrameState(frames[1]);
+    if (secondFrame.type === 'strike') {
+      return 10 + 10
+    }
+    if (secondFrame.type === 'notRolled') {
+      return 'pending'
+    }
+    return 10 + secondFrame.first
+  }
+
+  return 'pending';
 }
 
-function isFrameFinished(frame: Frame | undefined): frame is FinishedFrame {
-  if (frame === undefined) return false;
-  if (frame[0] === null) return false;
-  if (frame[0] === 10) return true;
-  if (frame[1] === null) return false;
-  return true;
+
+type FrameState =
+  | { type: 'notRolled'}
+  | { type: 'inProgress'; first: number }
+  | { type: 'open'; first: number; second: number }
+  | { type: 'spare'; first: number; second: number }
+  | { type: 'strike' };
+
+function determineFrameState(frame: Frame): FrameState {
+  if (frame[0] === 10) {
+    return { type: 'strike' };
+  };
+  if (frame[1] === null) {
+    return { type: 'inProgress', first: frame[0] }
+  };
+  if (typeof frame[1] === 'number') {
+    const isSpare = frame[0] + frame[1] === 10;
+    if (isSpare === true) {
+      return {
+        type: 'spare', first: frame[0], second: frame[1]
+      }
+    }
+    return {
+      type: 'open', first: frame[0], second: frame[1]
+    }
+
+  }
+  return { type: 'notRolled' }
 }
