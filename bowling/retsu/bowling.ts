4529339e6fb72c1a6390ce86ae23821a4e599ff2
cl-r-yamamoto <r-yamamoto@creationline.com>
2026-08-30 18:50:47 +0900

    switch文を使うようにリファクタリング

--- bowling/retsu/bowling.ts
+++ bowling/retsu/bowling.ts
@@ -2,72 +2,66 @@ type Frame = [number, number | null];
 
 type CalcResult = number | "pending";
 export function frameCalc(frames: Frame[]): CalcResult {
-  if (frames[0] === undefined) return "pending"
   const firstFrame = determineFrameState(frames[0]);
 
-  if (firstFrame.type === 'notRolled') {
-    return 'pending'
-  }
-
-  // ストライクの時
-  if (firstFrame.type === 'strike') {
-    if (frames[1] === undefined) return "pending"
-    const secondFrame = determineFrameState(frames[1]);
-    if (secondFrame.type === 'open' || secondFrame.type === 'spare') {
-      return 10 + secondFrame.first + secondFrame.second
+  switch (firstFrame.type) {
+    case "notRolled":
+      return "pending";
+    case "inProgress":
+      return "pending";
+    case "open":
+      return firstFrame.first + firstFrame.second;
+    case "spare": {
+      const secondFrame = determineFrameState(frames[1]);
+      if (secondFrame.type === "notRolled") {
+        return "pending";
+      }
+      if (secondFrame.type === "strike") {
+        return 10 + 10;
+      }
+      return 10 + secondFrame.first;
     }
-    return "pending";
-  }
-  // まだ未投入のとき
-  if (firstFrame.type === 'inProgress') {
-    return "pending";
-  }
-  if (firstFrame.type === 'open') {
-    return firstFrame.first + firstFrame.second
-  }
-
-  // スペアの時
-  if (firstFrame.type === 'spare') {
-    if (frames[1] === undefined) return "pending"
-    const secondFrame = determineFrameState(frames[1]);
-    if (secondFrame.type === 'strike') {
-      return 10 + 10
+    case "strike": {
+      const secondFrame = determineFrameState(frames[1]);
+      if (secondFrame.type === "open" || secondFrame.type === "spare") {
+        return 10 + secondFrame.first + secondFrame.second;
+      }
+      return "pending";
     }
-    if (secondFrame.type === 'notRolled') {
-      return 'pending'
-    }
-    return 10 + secondFrame.first
   }
-
-  return 'pending';
 }
 
-
 type FrameState =
-  | { type: 'notRolled'}
-  | { type: 'inProgress'; first: number }
-  | { type: 'open'; first: number; second: number }
-  | { type: 'spare'; first: number; second: number }
-  | { type: 'strike' };
+  | { type: "notRolled" }
+  | { type: "inProgress"; first: number }
+  | { type: "open"; first: number; second: number }
+  | { type: "spare"; first: number; second: number }
+  | { type: "strike" };
 
-function determineFrameState(frame: Frame): FrameState {
+function determineFrameState(frame: Frame | undefined): FrameState {
+  if (frame === undefined) {
+    return { type: "notRolled" };
+  }
   if (frame[0] === 10) {
-    return { type: 'strike' };
-  };
+    return { type: "strike" };
+  }
   if (frame[1] === null) {
-    return { type: 'inProgress', first: frame[0] }
-  };
-  if (typeof frame[1] === 'number') {
+    return { type: "inProgress", first: frame[0] };
+  }
+  if (typeof frame[1] === "number") {
     const isSpare = frame[0] + frame[1] === 10;
     if (isSpare === true) {
       return {
-        type: 'spare', first: frame[0], second: frame[1]
-      }
+        type: "spare",
+        first: frame[0],
+        second: frame[1],
+      };
     }
     return {
-      type: 'open', first: frame[0], second: frame[1]
-    }
-
+      type: "open",
+      first: frame[0],
+      second: frame[1],
+    };
   }
-  return { type: 'notRolled' }
+  return { type: "notRolled" };
 }
