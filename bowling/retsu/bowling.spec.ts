6a3249797fce0787a7b2271366063603b0a7b2bf
cl-r-yamamoto <r-yamamoto@creationline.com>
2026-08-30 16:28:51 +0900

    testをリファクタリング

--- bowling/retsu/bowling.spec.ts
+++ bowling/retsu/bowling.spec.ts
@@ -2,16 +2,18 @@ import { describe, it, expect } from "vitest";
 import { frameCalc } from "./bowling";
 
 describe("frameCalc", () => {
-  it("[[1,2]]のときは3", () => {
-    const result = frameCalc([[1, 2]]);
-    expect(result).toBe(3);
-  });
-  it("[[1,5],[3,4]]のときは6", () => {
-    const result = frameCalc([
-      [1, 5],
-      [3, 4],
-    ]);
-    expect(result).toBe(6);
+  describe("役なしのとき", () => {
+    it("[[1,2]]のときは3", () => {
+      const result = frameCalc([[1, 2]]);
+      expect(result).toBe(3);
+    });
+    it("[[1,5],[3,4]]のときは6", () => {
+      const result = frameCalc([
+        [1, 5],
+        [3, 4],
+      ]);
+      expect(result).toBe(6);
+    });
   });
   describe("スペアのとき", () => {
     it("[[1,9],[7,1]]のときは17", () => {
