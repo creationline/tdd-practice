8ca3aecbeec19a6b2c8b245120177288c8d223ad
cl-r-yamamoto <r-yamamoto@creationline.com>
2026-08-30 14:35:12 +0900

    [[1,5],[3,4]]のときは6

--- bowling/retsu/bowling.spec.ts
+++ bowling/retsu/bowling.spec.ts
@@ -1,10 +1,16 @@
 import { describe, it, expect } from "vitest";
 import { frameCalc } from "./bowling";
 
-
 describe("frameCalc", () => {
   it("[[1,2]]のときは3", () => {
-    const result = frameCalc([[1,2]])
+    const result = frameCalc([[1, 2]]);
     expect(result).toBe(3);
+  });
+  it("[[1,5],[3,4]]のときは6", () => {
+    const result = frameCalc([
+      [1, 5],
+      [3, 4],
+    ]);
+    expect(result).toBe(6);
   });
 });
--- bowling/retsu/bowling.ts
+++ bowling/retsu/bowling.ts
@@ -1,3 +1,8 @@
-export function frameCalc(frames: number[][]): number {
-  return 3
+export function frameCalc(frames: [number, number][]): number {
+  const firstFrame = frames[0];
+  if (firstFrame === undefined) {
+    throw Error("Frame Not Found");
+  }
+  const result = firstFrame[0] + firstFrame[1];
+  return result;
 }
