1652b14f9c6effe6d8506bcaaecab93e79230b80
m-yamada-cl <m-yamada@creationline.com>
2026-09-14 02:38:53 +0000

    2フレーム分、入力できる

--- bowling/team2/bowling.test.ts
+++ bowling/team2/bowling.test.ts
@@ -3,7 +3,11 @@ import { bowling } from "./bowling"
 
 describe("bowling", () => {
   it("1フレーム2回投げる場合、合計スコアを返す", () => {
-    expect(bowling(0,0)).toBe("0"),
-    expect(bowling(4,5)).toBe("9")
+    expect(bowling([[0,0]])).toBe(0),
+    expect(bowling([[4,5]])).toBe(9)
+  })
+
+  it("2フレーム分、入力できる", () => {
+    expect(bowling([[0,0],[0,1]])).toBe(1)
   })
 })
--- bowling/team2/bowling.ts
+++ bowling/team2/bowling.ts
@@ -1,3 +1,5 @@
-export function bowling(number1: number,number2: number): number {
-    return number1+ number2
-  }
+export function bowling(
+  frames: number[][],
+): number {
+  return frames.flat().reduce((sum, pins) => sum + pins, 0)
+}
