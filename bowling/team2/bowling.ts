04de07f1967499c27c7ca32a7b9c7749d22284ac
m-yamada <m-yamada@cladmins-MacBook-Air-2.local>
2026-09-24 14:08:23 +0900

    スペアの場合、次の1投のスコアを加算する

--- bowling/team2/bowling.ts
+++ bowling/team2/bowling.ts
@@ -1,5 +1,23 @@
-export function bowling(
-  frames: number[][],
-): number {
-  return frames.flat().reduce((sum, pins) => sum + pins, 0)
+type Frame = [first: number, second?: number]
+
+export function bowling(frames: Frame[]): number {
+  let score = 0
+  for (let i = 0; i < frames.length; i++) {
+    const frame = frames[i]
+    if (frame === undefined || frame[1] === undefined) {
+      continue
+    }
+    const first = frame[0]
+    const second = frame[1]
+    score += first + second
+
+    if (first + second === 10) {
+      const nextFrame = frames[i + 1]
+      if(nextFrame !== undefined){
+        score += nextFrame[0]
+      }
+    }
+  }
+  return score
 }
+ 
